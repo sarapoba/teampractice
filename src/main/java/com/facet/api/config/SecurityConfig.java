22ae40bb48c96e91522476638da33a37332048d5
dwg245 <dwg0245@gmail.com>
2026-03-03 16:42:11 +0900

    [feat] 시큐리티를 이용한 소셜 로그인 - 카카오

--- src/main/java/com/facet/api/config/SecurityConfig.java
+++ src/main/java/com/facet/api/config/SecurityConfig.java
@@ -21,6 +21,8 @@ package com.facet.api.config;
 */
 
 import com.facet.api.config.filter.JwtFilter;
+import com.facet.api.config.oauth2.OAuth2AuthenticationSuccessHandler;
+import com.facet.api.user.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +41,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
+    private final OAuth2UserService oAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -53,6 +57,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.oauth2Login(config -> {
+            config.userInfoEndpoint(
+                    endpoint ->
+                            endpoint.userService(oAuth2UserService)
+            );
+            config.successHandler(oAuth2AuthenticationSuccessHandler);
+        });
+
         http.authorizeHttpRequests(
                 (auth) -> auth
                         .requestMatchers("/user/login", "/user/signup", "/user/verify").permitAll()
--- src/main/java/com/facet/api/user/model/UserDto.java
+++ src/main/java/com/facet/api/user/model/UserDto.java
@@ -3,7 +3,47 @@ package com.facet.api.user.model;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Map;
+
 public class UserDto {
+
+    @Getter
+    @Builder
+    // 소셜 로그인
+    public static class OAuth {
+        private String email;
+        private String name;
+        private String provider;
+        private boolean enable;
+        private String role;
+
+        public static OAuth from(Map<String, Object> attributes, String provider) {
+            String providerId = ((Long) attributes.get("id")).toString();
+            System.out.println(providerId);
+
+            String email = providerId + "@kakao.social";
+            Map properties = (Map) attributes.get("properties");
+            String name = (String) properties.get("nickname");
+
+            return OAuth.builder()
+                    .email(email)
+                    .name(name)
+                    .provider(provider)
+                    .enable(true)
+                    .role("ROLE_USER")
+                    .build();
+        }
+
+        public User toEntity() {
+            return User.builder()
+                    .email(this.email)
+                    .name(this.name)
+                    .password(this.provider)
+                    .enable(this.enable)
+                    .role(this.role)
+                    .build();
+        }
+    }
 
     @Getter
     public static class SignupReq {
